f1f8c1d918d72254b4d2653f7ce27dcdea171e09
Austin Beeler <abeeler1@uncc.edu>
2017-01-11 23:36:22 -0500

    Actually add basic stamp game functionality

---
+++ Assets/Scripts/StampStadium/StampSpace.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StampSpace : MonoBehaviour {
+	private SpriteRenderer spriteRenderer;
+	
+	public void SetColor(Color color) {
+		if (spriteRenderer == null)
+			spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
+		spriteRenderer.color = color;
+	}
+}
