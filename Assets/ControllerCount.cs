6cf87a77f750a563c2d38b1f9cc874c5201337e1
Austin Beeler <abeeler1@uncc.edu>
2017-01-13 18:58:57 -0500

    Add testing scene for multiple controllers
    No steam controller support yet (spent so long trying)

---
+++ Assets/ControllerCount.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerCount : MonoBehaviour {
+	private TextMesh text;
+
+	void Start() {
+		text = GetComponent<TextMesh> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string newText = "";
+		foreach (string gamepadName in Input.GetJoystickNames())
+			newText += gamepadName + "\n";
+		text.text = newText;
+	}
+}
---
+++ Assets/Scripts/Test/InputTest.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputTest : MonoBehaviour {
+	private static InputTest nextFree = null;
+
+	public int order = 0;
+	public GamepadInput gamepad = null;
+
+	private SpriteRenderer spriteRender;
+	private Vector3 startPosition;
+
+	void Start() {
+		if (nextFree == null || nextFree.order > order)
+			nextFree = this;
+		spriteRender = GetComponent<SpriteRenderer> ();
+		spriteRender.enabled = false;
+		startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gamepad == null) {
+			if (nextFree == null || nextFree.order > order)
+				nextFree = this;
+			if (this == nextFree) {
+				gamepad = GamepadInput.Get (2);// GamepadInput.AllGamepads.Find (controller => !controller.InUse && Input.GetButton (controller.Start));
+				if (gamepad != null && Input.GetButton(gamepad.Start)) {
+					nextFree = null;
+					gamepad.Lock ();
+					spriteRender.enabled = true;
+				}
+			}
+		}
+
+		if (gamepad != null) {
+			Color targetColor = Color.white;
+			if (Input.GetButton (gamepad.A))
+				targetColor = Color.green;
+			else if (Input.GetButton (gamepad.X))
+				targetColor = Color.blue;
+			else if (Input.GetButton (gamepad.Y))
+				targetColor = Color.yellow;
+			else if (Input.GetButton (gamepad.B))
+				targetColor = Color.red;
+			
+			spriteRender.color = targetColor;
+			transform.position = startPosition + new Vector3 (Input.GetAxis (gamepad.LeftHorizontal), -Input.GetAxis (gamepad.LeftVertical));
+
+			if (Input.GetButton (gamepad.Back)) {
+				gamepad.Free ();
+				spriteRender.enabled = false;
+				gamepad = null;
+			}
+		}
+	}
+}
