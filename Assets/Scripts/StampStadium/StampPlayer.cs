e525763d6d97724d268a52d7dacd566840b842b0
ZalThulu <rcopperheads11@aol.com>
2017-01-14 00:32:13 -0500

    added first iteration of walling for ghost (don't press the x button)

--- Assets/Scripts/StampStadium/StampPlayer.cs
+++ Assets/Scripts/StampStadium/StampPlayer.cs
@@ -1,18 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class StampPlayer : MonoBehaviour {
 
 	public float movementDelay;
-	public int size = 1;
+    
+    public int size = 1;
 	public Color playerColor = Color.cyan;
 	public Grid gameGrid;
-
+    public bool isGhost = false;
 	private float delayTimer = 0;
 	private int x, y;
+    private float nextWall;
+    public float wallDelay;
 
-	// Update is called once per frame
-	void Start () {
+    // Update is called once per frame
+    void Start () {
 		MoveTo (Random.Range (gameGrid.Left, gameGrid.Right - size), Random.Range (gameGrid.Bottom, gameGrid.Top - size));
 	}
 
@@ -29,6 +33,15 @@ public class StampPlayer : MonoBehaviour {
 			else if (Input.GetKey (KeyCode.D))
 				MoveTo (x + 1, y);
 		}
+
+        //updates specific to boss player
+        if (isGhost){
+            if (Input.GetKey(KeyCode.X) && Time.time > nextWall){
+                nextWall = Time.time + wallDelay;
+                BossWall();
+            }
+
+        }
 	}
 
 	void MoveTo(int x, int y)	{
@@ -42,7 +55,139 @@ public class StampPlayer : MonoBehaviour {
 		this.x = x;
 		this.y = y;
 		this.delayTimer = movementDelay;
-		foreach (StampSpace square in targetSpaces)
-			square.SetColor (playerColor);
+
+        if (!isGhost){
+            foreach (StampSpace square in targetSpaces)
+                square.SetColor(playerColor);
+        }
 	}
+    
+    //function that generates a wall of random length in random positions
+    void BossWall(){
+        //values that represents the total number of walls to create and the max possible size of said walls.
+        int wallsToCreate = 4;
+        int wallsMaxSize = 6;
+        int wallsMinSize = 2;
+
+        //values that represents the 'tendency' for the rng to lean towards a particular end of the range of wallMaxSize
+        //the chance to re-roll if the max wall size falls into the half of the bound designated by the value. -1 = lower, 1 = upper, 0 = none 
+        float wallSizeRollChance = 0.2f;
+        float wallSizeRollValue = 0;
+
+        //value that represents the duration of each wall
+        int wallDuration = 10;
+
+        for (int i=0; i<wallsToCreate; i++){
+Debug.Log("Creating wall: " + (i + 1) + " of " + wallsToCreate);
+            bool wallGenerated = false;
+            Random.InitState(System.DateTime.Now.Millisecond);
+int whileBreaker1 = 0;
+            while (!wallGenerated){
+whileBreaker1++;
+if(whileBreaker1 == 50) { break; }
+                //First, pick a random point in the world within the bounds of the grid
+                Vector3 startingPoint = new Vector3(Random.Range(0.0f,gameGrid.Height), 0.0f, Random.Range(0.0f, gameGrid.Width));
+Debug.Log("Trying StartingPoint: " + startingPoint + " for wall Number: " + (i + 1));
+                //check if that point is already a wall
+                GameObject tempPointCheck = null;
+                if (gameGrid.TryGetGridObject(startingPoint, out tempPointCheck) && !tempPointCheck.GetComponent<StampSpace>().isWall){
+Debug.Log("StartingPoint: " + startingPoint + " for wall Number: " + (i + 1) + " VALID");
+                    //we have a valid point that is not a wall.  Pick a random direction to wall off
+                    int chosenDirection = 0;
+                    List<int> directionsChecked = new List<int>();
+int whileBreaker2 = 0;
+                    while (chosenDirection == 0){
+whileBreaker2++;
+if (whileBreaker2 == 50) { break; }
+                        int tempDirection = Random.Range(1, 4);
+Debug.Log("Testing Direction: " + tempDirection + " For StartingPoint: " + startingPoint + " for wall Number: " + (i + 1));
+                        if (!directionsChecked.Contains(tempDirection)){
+Debug.Log("Direction: " + tempDirection + " For StartingPoint: " + startingPoint + " for wall Number: " + (i + 1) + " VALID");
+                            directionsChecked.Add(tempDirection);
+                            //check that we can actually build a minimum length wall in that direction
+Debug.Log("Testing min wall in Direction: " + tempDirection + " For StartingPoint: " + startingPoint + " for wall Number: " + (i + 1));
+                            for (int j = 0; j < wallsMinSize; j++){
+                                //temporarily select a gridspace in the chose direction
+                                Vector3 tempWallPoint;
+                                if (tempDirection == 1)
+                                    tempWallPoint = new Vector3(startingPoint.x, startingPoint.y, startingPoint.z + j); //up
+                                else if (tempDirection == 2)
+                                    tempWallPoint = new Vector3(startingPoint.x + j, startingPoint.y, startingPoint.z); //right
+                                else if (tempDirection == 3)
+                                    tempWallPoint = new Vector3(startingPoint.x, startingPoint.y, startingPoint.z - j); //down
+                                else
+                                    tempWallPoint = new Vector3(startingPoint.x - j, startingPoint.y, startingPoint.z); //left
+
+                                //ensure that we can build a wall in the temporary grid space, if not try a different direction
+                                GameObject tempMinWall = null;
+                                if (!gameGrid.TryGetGridObject(tempWallPoint, out tempMinWall))
+                                    break;
+Debug.Log("Testing min wall: " + (j+1) + " of " + wallsMinSize +  " in Direction: " + tempDirection + " for wall Number: " + (i + 1) + " VALID");
+                            }
+                        }
+                        else{
+                            //probably a better way to do this, but this ensures directions that are not valid will not be marked true.
+                            //for the time being, don't make this an else if or stuff breaks
+                            if (directionsChecked.Count == 4) { break; }
+                        }
+
+                        //if we made it this far, then we know the current direction is valid
+                        chosenDirection = tempDirection;
+                    }
+
+                    if (chosenDirection != 0)
+                    {
+Debug.Log("Wall pass checks, building wall: " + (i + 1) + " of " + wallsToCreate);
+                        //generate the wallSize
+                        int wallSize = wallsMaxSize / 2;
+                        bool pickAgain = false;
+int whileBreaker3 = 0;
+                        do
+                        {
+whileBreaker3++;
+if (whileBreaker3 == 50) { break; }
+Debug.Log("Generating wall size...");
+                            pickAgain = false;
+                            wallSize = Random.Range(wallsMinSize, wallsMaxSize);
+                            //if the value is in the middle always use it
+                            if (wallSizeRollValue == 1 && wallSize > (wallsMaxSize / 2) && Random.value <= wallSizeRollChance)
+                                pickAgain = true;
+                            if (wallSizeRollValue == -1 && wallSize < (wallsMaxSize / 2) && Random.value <= wallSizeRollChance)
+                                pickAgain = true;
+                        } while (pickAgain);
+Debug.Log("Final Wall Size: " + wallSize);
+                        //build a list of all the spaces of the wall in the chosen direction
+                        List<StampSpace> wallSpaces = new List<StampSpace>();
+                        for (int w = 0; w < wallSize; i++)
+                        {
+                            //temporarily select a gridspace in the chose direction
+                            Vector3 tempWallPoint;
+                            if (chosenDirection == 1)
+                                tempWallPoint = new Vector3(startingPoint.x, startingPoint.y, startingPoint.z + w); //up
+                            else if (chosenDirection == 2)
+                                tempWallPoint = new Vector3(startingPoint.x + w, startingPoint.y, startingPoint.z); //right
+                            else if (chosenDirection == 3)
+                                tempWallPoint = new Vector3(startingPoint.x, startingPoint.y, startingPoint.z - w); //down
+                            else
+                                tempWallPoint = new Vector3(startingPoint.x - w, startingPoint.y, startingPoint.z); //left
+Debug.Log("Getting Final wall point: " + tempWallPoint + " Of Wall: " + (i+1));
+                            //ensure that we can build a wall in the temporary grid space, if not try a different direction
+                            GameObject tempMinWall = null;
+                            if (gameGrid.TryGetGridObject(tempWallPoint, out tempMinWall))
+                                wallSpaces.Add(tempMinWall.GetComponent<StampSpace>());
+Debug.Log("Wall point: " + tempWallPoint + " Of Wall: " + (i + 1) + "ADDED to wallSpaces");
+                        }
+
+                        //loop over our list of wall spaces and turn each into a wall
+                        foreach (StampSpace square in wallSpaces)
+                            square.InvokeWall(wallDuration);
+                        
+                        //wall has been generated    
+                        wallGenerated = true;
+Debug.Log("Wall: " + (i + 1) + " of " + wallsToCreate + " BUILT");
+                    }
+                }
+            }
+        }
+    }
 }
--- Assets/Scripts/StampStadium/StampSpace.cs
+++ Assets/Scripts/StampStadium/StampSpace.cs
@@ -3,10 +3,30 @@ using System.Collections;
 
 public class StampSpace : MonoBehaviour {
 	private SpriteRenderer spriteRenderer;
-	
+    public bool isWall = false;
+
 	public void SetColor(Color color) {
 		if (spriteRenderer == null)
 			spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
 		spriteRenderer.color = color;
 	}
+
+    public void InvokeWall(int wallDuration){
+        StartCoroutine(ActiveWall(wallDuration));
+    }
+
+    IEnumerator ActiveWall(int wallDuration)
+    {
+        isWall = true;
+        float startTime = Time.time;
+        Color startColor = spriteRenderer.color;
+        spriteRenderer = null; 
+        SetColor(Color.black);
+        while (Time.time - startTime < wallDuration){
+            yield return new WaitForFixedUpdate();
+        }
+        spriteRenderer = null;
+        SetColor(startColor);
+        isWall = false;
+    }
 }
